37242e3b552538722810c76b9ead5c3b30a6d74b
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-06-05 12:21:28 +0200

    added image assets and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
-import { useState } from 'react'
+import React,{useEffect, useState} from 'react'
+import Navbar from './components/Navbar';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [size, setSize] = useState(window.innerWidth)
+  const [menuOpen, setMenuOpen] = useState(false)
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize(window.innerWidth)
+      if(size > 640){
+        setMenuOpen(false)
+      }
+    }
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  },[size])
 
   return (
-    <div className='bg-amber-400'>
-      <div className='bg-amber-400'>
-        <a href="https://vite.dev" target="_blank">
-         
-        </a>
-        <a href="https://react.dev" target="_blank">
-         
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Navbar size={size} menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import logo from '../assets/images/logo.svg'
+import close from '../assets/images/icon-close-menu.svg'
+import hamburger from '../assets/images/icon-hamburger.svg'
+
+
+const Navbar = ({size, menuOpen, setMenuOpen}) => {
+  return (
+    <div  className='py-3 px-2 sm:px-5 md:px-10 lg:px-24 bg-green-600 flex align-center justify-between relative'>
+        <img src={logo} alt="" className='size-auto'/>
+
+        {
+            size > 640 ? (
+                <nav>
+                    <ul className={`flex items-center gap-5 text-gray-50`}>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Discover</a></li>
+                        <li><a href="#">GetStarted</a></li>
+                    </ul>
+                </nav>
+            ) : 
+            (
+                <nav className={`${menuOpen ? 'absolute top-12 bg-white w-11/12 left-1/2 -translate-x-1/2 rounded-md' : 'hidden'}`}>
+                    <ul className={`${menuOpen ? 'flex flex-col  justify-center text-center' : ''}`}>
+                        <li className={`${menuOpen ? 'py-5 border-b border-gray-200 font-semibold' : ''}`}><a href="#">About</a></li>
+                        <li className={`${menuOpen ? 'py-4 border-b border-gray-200 font-semibold' : ''}`}><a href="#">Discover</a></li>
+                        <li className={`${menuOpen ? 'py-4 font-semibold' : ''}`}><a href="#">GetStarted</a></li>
+                    </ul>
+                </nav>
+            )
+        }  
+        <div className={`${size > 640 ? 'hidden' : 'block'}`}>
+            { menuOpen ? 
+                <img src={close} alt="" className='cursor-pointer' onClick={() => setMenuOpen(false)}/> : 
+                <img src={hamburger} alt="" className='cursor-pointer' onClick={() => setMenuOpen(true)}/>
+            }
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
